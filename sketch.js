9458b336bf86c13b219144bf0e6f75fa823b7514
molklyplp <molklyplp@gmail.com>
2024-05-08 10:56:25 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+ createCanvas(400, 400,WEBGL);
+ rotateX(PI/4)
+ rotateY(PI/4)
+ normalMaterial();
+box(200,200,200)
+}
+function draw() {
+   background('yellow')
+  push();
+ rotateZ(frameCount * 0.05);
+ rotateX(frameCount * 0.03);
+ rotateY(frameCount * 0.05);
+box(200,200,200)
+}
+
+
+
